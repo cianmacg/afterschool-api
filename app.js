0ce1c8461862a7b274532eaf3af29515d83d6a5f
cianmacg <scian94@gmail.com>
2024-04-19 18:18:41 +0100

    Initial
    Copy of previous project not on git. Simple api using express to add/delete/change excel file. Tracking when children enter/exit the afterschool.

---
+++ app.js
@@ -0,0 +1,173 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const { spawn } = require('child_process');
+const math_py = 'scripts/math-stuff.py'
+const sheets_py = 'scripts/sheets.py'
+
+const sheets_path = __dirname + '\\public\\sheets\\';
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// This path is used to add a new child to the database. It must be provided with a Name
+app.post('/add', (req, res) => {
+  if(req.query.name == "" || req.query.name == null){
+    res.sendStatus(422);
+  }
+  else {
+    add_new_kid(spawn('python', [sheets_py, "add_kid", sheets_path.concat("kids.xlsx"), req.query.name]), (err, kid) => {
+      if (err) {
+          return res.status(500).send("Error retrieving data");
+      }
+      res.sendStatus(201);
+    });
+  }
+});
+
+//This path is used to remove a child from the database. It must be provided with an ID
+app.post('/delete', (req, res) => {
+  if(req.query.id == "" || req.query.id == null){
+    res.sendStatus(422);
+  }
+  else {
+    delete_kid(spawn('python', [sheets_py, "delete_kid", sheets_path.concat("kids.xlsx"), req.query.id]), (err, kid) => {
+      if (err) {
+          return res.status(500).send("Error retrieving data");
+      }
+      res.sendStatus(201);
+    });
+  }
+});
+
+// This path is used to change the status of a child to In or Out (opposite of current status). It must be provided with an ID
+app.post('/change', (req, res) => { 
+  if(req.query.id == "" || req.query.id == null){
+    res.sendStatus(422);
+  }
+  else {
+    change_status(spawn('python', [sheets_py, "change_status", sheets_path.concat("kids.xlsx"), req.query.id]), (err, kid) =>{
+      if (err) {
+          return res.status(500).send("Error retrieving data");
+      }
+      res.sendStatus(201);
+    });
+  }
+});
+
+// This path is used to get a list of all children and their statuses. 
+app.get('/get', (req, res) => {
+  var data = {
+    'title' : 'Express',
+    'kids' : ''
+  }
+  get_kids(spawn('python', [sheets_py, "get_kids", sheets_path.concat("kids.xlsx")]), (err, kids) => {
+    if (err) {
+        return res.status(500).send("Error retrieving data");
+    }
+    data.kids = kids;
+    res.json(data);
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// We use this function to add a new child to the database.
+function add_new_kid(kid, cb) {
+
+  kid.stdout.on('data', (data) => {
+    // Assuming the Python script returns JSON
+    const result = JSON.parse(data);
+    console.log(result);
+    return cb(null, result)
+  });
+  
+  kid.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    return cb(true, null)
+  });
+  
+  kid.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+function delete_kid(kid, cb){
+  kid.stdout.on('data', (data) => {
+    // Assuming the Python script returns JSON
+    const result = JSON.parse(data);
+    console.log(result);
+    return cb(null, result)
+  });
+  
+  kid.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    return cb(true, null)
+  });
+  
+  kid.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+// We use this function to change the status of a child to In or Out (the opposite of current status).
+function change_status(kid, cb) {
+
+  kid.stdout.on('data', (data) => {
+    // Assuming the Python script returns JSON
+    const result = JSON.parse(data);
+    console.log(result);
+    return cb(null, result);
+  });
+  
+  kid.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    return cb(true, null);
+  });
+  
+  kid.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+// We use this function to get the full list of children and their statuses.
+function get_kids(kid, cb) {
+  kid.stdout.on('data', (data) => {
+    // Assuming the Python script returns JSON
+    const result = JSON.parse(data);
+    return cb(null, result);
+  });
+  
+  kid.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    return { "result" : "Error.", "ID" : "", "Name" : "", "Status" : "" };
+  });
+  
+  kid.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+module.exports = app;
---
+++ scripts/sheets.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import sys
+import json
+
+#Add a new child to the database.
+def add_new_kid(path, value):
+    kids_df = pd.read_excel(path)
+    
+    id = pd.to_numeric(kids_df['ID'].iloc[-1]) + 1
+    kids_df.loc[len(kids_df.index)] = [id, value, 'Out']
+    kids_df.to_excel(path, index=False)
+    #return json.dumps({"result" : kids_df.iloc[-1].to_json() })
+    return kids_df.to_json()
+
+#Delete a child from the database based on their ID
+def delete_kid(path, kid):
+    kids_df = pd.read_excel(path)
+    kid = int(kid)
+    
+    if kid not in kids_df['ID'].values:
+        return json.dumps({ 'result' : 'Unable to delete. ID does not exist.'})
+    
+    kids_df.drop(kids_df[kids_df['ID'] == kid].index, inplace=True)
+    kids_df.to_excel(path, index=False)
+    
+    return kids_df.to_json()
+
+#Changes the status of the ID "Kid" to the opposite of its current status.
+def change_status(path, kid):
+    kids_df = pd.read_excel(path, sheet_name="Kids")
+    log_df = pd.read_excel(path, sheet_name="Log")
+    kid = int(kid)
+    #Make sure the ID exists in our database, otherwise we have to tell the user it doesn't exist
+    if kid not in kids_df['ID'].values:
+        return json.dumps({ "result" : "This ID does not exist in the database."})
+    
+    kid_index = kids_df[kids_df["ID"] == kid].index
+    #If the childs status = In, change it to Out
+    if kids_df.at[kid_index[0], 'Status'] == 'In':
+        kids_df.at[kid_index[0], 'Status'] = 'Out'
+        
+    #If the childs status = Out, change it to In
+    elif kids_df.at[kid_index[0], 'Status'] == 'Out':
+        kids_df.at[kid_index[0], 'Status'] = 'In'
+    
+    kids_df.to_excel(path, index=False)
+    #return kids_df.to_json()
+    return json.dumps({"result" : {
+            'Name' : kids_df.at[kid_index[0], 'Name'],
+            'New Status' : kids_df.at[kid_index[0], 'Status']
+            }
+        })
+
+#Return a list of all children in the database and their statuses.
+def get_kids(path):
+    return pd.read_excel(path).to_json(orient = "records")
+
+if __name__ == "__main__":
+    try:
+        to_do = sys.argv[1]
+        path = sys.argv[2]
+    except:
+        print(json.dumps({"result" : "Not enough arguments. Minimum 2 required (Function and path to excel file)."}))
+        sys.exit()
+        
+    if to_do == "add_kid":
+        try:
+            value = sys.argv[3]
+        except:
+            print(json.dumps({"result" : "This function requires a kid name. Please provide it after the path to the excel file."}))
+            sys.exit()
+            
+        print(add_new_kid(path, value))
+    
+    elif to_do == "delete_kid":
+        try:
+            value = sys.argv[3]
+        except:
+            print(json.dumps({"result" : "This function requires a kid ID. Please provide it after the path to the excel file."}))
+            sys.exit()
+            
+        print(delete_kid(path, value))
+    
+    elif to_do == "change_status":
+        try:
+            value = sys.argv[3]
+        except:
+            print(json.dumps({"result" : "This function requires a kid ID. Please provide it after the path to the excel file."}))
+            sys.exit()
+            
+        print(change_status(path, value))
+        
+    elif to_do == "get_kids":
+        print(get_kids(path))
+        
+    else:
+        print(json.dumps({ "result" : "Unknown function name." }))
