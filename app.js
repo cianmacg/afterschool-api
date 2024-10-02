9fb5fbffcf3d2b10e2d0a9d9d3e5f7a3237a2837
cianmacg <scian94@gmail.com>
2024-10-02 18:46:53 +0100

    changed app.use('cors') to the correct app.use(cors())

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('cors')
+app.use(cors());
 
 // This path is used to add a new child to the database. It must be provided with a Name
 app.put('/add', (req, res) => {
